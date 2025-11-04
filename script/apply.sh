06ca675c52da9f873163013af95f8f66f49262f3
ygd <852383409@qq.com>
2025-11-04 10:10:45 +0000

    update

---
+++ script/apply.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+  echo "apply init config"
+else 
+  echo "check argv"
+fi
+
+
---
+++ script/install.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install neovim
+
+
